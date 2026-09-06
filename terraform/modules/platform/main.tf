10a403bf6ef778482d3705e0c1645898298e892b
OBINNA OBIKA <bilolo74@gmail.com>
2026-09-06 09:14:37 -0400

    fix: format Terraform platform module

--- terraform/modules/platform/main.tf
+++ terraform/modules/platform/main.tf
@@ -1,15 +1,32 @@
-variable "name" { type = string }
-variable "subnet_ids" { type = list(string) }
-variable "tags" { type = map(string) }
+variable "name" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
 
 resource "aws_security_group" "platform" {
   name   = "${var.name}-platform"
   vpc_id = data.aws_subnet.selected.vpc_id
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = var.tags
 }
 
-data "aws_subnet" "selected" { id = var.subnet_ids[0] }
+data "aws_subnet" "selected" {
+  id = var.subnet_ids[0]
+}
 
 resource "aws_cloudwatch_log_group" "platform" {
   name              = "/platform/${var.name}"
@@ -17,4 +34,6 @@ resource "aws_cloudwatch_log_group" "platform" {
   tags              = var.tags
 }
 
-output "security_group_id" { value = aws_security_group.platform.id }
+output "security_group_id" {
+  value = aws_security_group.platform.id
+}
