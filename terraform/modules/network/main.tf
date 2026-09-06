555429ae808db8ff91aa55f547c3a7dff633b2b6
OBINNA OBIKA <bilolo74@gmail.com>
2026-09-06 09:17:06 -0400

    fix: match terraform fmt alignment

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = cidrsubnet(var.cidr, 4, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  tags               = merge(var.tags, { Name = "${var.name}-private-${count.index + 1}" })
+  tags              = merge(var.tags, { Name = "${var.name}-private-${count.index + 1}" })
 }
 
 data "aws_availability_zones" "available" {
